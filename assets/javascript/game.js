b853bef8e05dde82d7418923c193089c40a1f9eb
Leah Kurtz <lfkurtz7@gmail.com>
2019-10-22 18:03:13 -0400

    created simplified functions for GetRandomNumber, modified reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,64 @@
- 
-var targetNumber = Math.floor(Math.random() * 101) + 19;
-var wins = 0; 
-var losses = 0; 
+var targetNumber, numberOptions;
 var counter = 0;
+var wins = 0;
+var losses = 0;
 
-$("#number-to-guess").text(targetNumber);
-$(".wins").text(wins);
-$(".losses").text(losses);
-$(".totalScore").text(counter);
+reset();
 
-//Start();
 
-var numberOptions = [Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1];
-console.log(numberOptions);
+$(".crystal-image").on("click", function () {
+    var crystalValue = parseInt($(this).attr("data-crystalvalue"));
+    console.log(crystalValue)
+    counter += crystalValue;
 
-$(".crystal-image").each(function(i) {
-    $(this).attr("data-crystalvalue", numberOptions[i]);
-});
 
-$(".crystal-image").on("click", function() {
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-     crystalValue = parseInt(crystalValue);
-     console.log(crystalValue)
-     counter += crystalValue;
-     alert(" New score " + counter);
-     
     $(".totalScore").text(counter);
 
-     if (counter === targetNumber) {
-  alert("You win!")
-  wins++;
-  //Start();
-}
-
-else if (counter >= targetNumber) {
-  alert("You lose, try again!")
-  losses++;
-  //Start();
-}
-$(".wins").text(wins);
-$(".losses").text(losses);
+    if (counter === targetNumber) {
+        alert("You win!")
+        wins++;
+        reset();
+    }
+    else if (counter > targetNumber) { // should I use > instead of >= ?? //
+        alert("You lose, try again!")
+        losses++;
+        reset();
+    }
+    $(".wins").text(wins);
+    $(".losses").text(losses);
 
 });
 
+function reset() { // something isnt right with this //
+    targetNumber = GetRandomNumber(19, 120);
+    counter = 0;
+    $("#number-to-guess").text(targetNumber);
+    $(".wins").text(wins);
+    $(".losses").text(losses);
+    $(".totalScore").text(counter);
+
+    $(".crystal-image").each(function (i) {
+        $(this).attr("data-crystalvalue", GetRandomNumber(1, 12));
+    });
+}
+// function GetTargetNumber() {
+//     return Math.floor(Math.random() * 101) + 19
+// }
+// function GetNumberOptions() {
+//     var tempArr = [];
+//     for (var i = 0; i < 4; i++) {
+//         var rand = Math.floor(Math.random() * 12) + 1;
+//         tempArr.push(rand)
+//     }
+//     return tempArr
+// }
+
+function GetRandomNumber(low, high){
+    var difference = (high - low) + 1;
+    return Math.floor(Math.random() * difference) + low
+}
 
 
-// determine if its a win or loss, 
-// make sure game restarts when player wins or loses 
+
+// // make sure game restarts when player wins or loses 
+// // issue with losses function
